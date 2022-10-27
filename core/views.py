c22bef320b769604ec2ee2ac1983115c734ca6de
shc5529 <shc5529@naver.com>
2022-10-28 02:38:42 +0900

    feat: core/views.py에 main 함수 생성
    메인 페이지를 처리해주는 main 함수 생성
    추후에 이미지 슬라이드, (메인 페이지에서 사용할)할랄 이미지, (메인 페이지에서 사용할)알콜 이미지  models 생성후 주석 해제 필요

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from shop.models import Item
+
 # Create your views here.
+
+def main(request):
+	#TODO: 주석 처리된 부분 models 생성후 주석 해제해야됨
+
+	best_item = Item.objects.filter(best_item=True)[:12]
+    # carousel_list = Carousel.objects.all()
+    # main_img_halal = MainImages.objects.first()
+    # main_img_alcohol = MainImages.objects.last()
+
+	return render(request, 'shop/container/main', {
+		'product': best_item,
+        # 'carousel_list': carousel_list,
+        # 'main_img_halal': main_img_halal,
+        # 'main_img_alcohol': main_img_alcohol,
+	})
