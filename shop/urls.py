315ec4ba1a45fc0ade34d4fa2df5a72b4973cdd9
shc5529 <shc5529@naver.com>
2022-11-05 02:27:46 +0900

    refactor: shop/urls.py의 category_list_all, category_list를 re_path에서 path로 수정

--- shop/urls.py
+++ shop/urls.py
@@ -3,10 +3,7 @@ from shop import views
 
 app_name = 'shop'
 urlpatterns = [
-    re_path(r'^(?P<category_slug>[-\w]+)/all/$', 
-        views.CategoryListAllView.as_view(), name='category_list_all'),
-    re_path(r'^(?P<category_slug>[-\w]+)/(?P<sub_category_slug>[-\w]+)/$', 
-        views.CategoryListView.as_view(), name='category_list'),
-    path('<str:category_slug>/<str:sub_category_slug>/<str:item_slug>/',
-        views.item_detail, name='item_detail'),
+    path('<str:category_slug>/ALL/', views.CategoryListAllView.as_view(), name='category_list_all'),
+    path('<str:category_slug>/<str:sub_category_slug>/', views.CategoryListView.as_view(), name='category_list'),
+    path('<str:category_slug>/<str:sub_category_slug>/<str:item_slug>/', views.item_detail, name='item_detail'),
 ]
