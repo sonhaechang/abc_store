f97ac24ea0d449b3e023380a91a0708d35602fc2
shc5529 <shc5529@naver.com>
2022-11-24 00:11:56 +0900

    feat: create OrderItemHandler class
    1. OrderItemHandler 생성(주문상품을 핸들링)
    2. OrderItemHandler.create_order_items (주문 상품 생성)
    3. OrderItemHandler.set_paid_status (주문 상품의 상태를 order의 status(paid)로 변경)
    4. OrderItemHandler.set_cancel_status (주문 상품들의 status를 주문취소로 변경)
    5. OrderItemHandler.stock_minus_counter (상품의 수량을 주문한 상품의 수만큼 차감)
    6. OrderItemHandler..stock_plus_counter (상품의 수량을 주문 취소한 수만큼 가감)
    
    style: order.services.OrderInfo에 주석 추가

--- order/services.py
+++ order/services.py
@@ -1,15 +1,21 @@
 from uuid import uuid4
-from datetime import datetime, timedelta
 
-from order.models import Order
+from django.db.models import F
+
+from order.models import Order, OrderItem
+
 
 class OrderInfo:
+	''' 주문정보 생성하는 class '''
+
 	def __init__(self, request, order_items):
 		self.request = request
 		self.order_items = order_items
 		self.amount = sum(order_item.amount for order_item in order_items)
 
 	def get_instance(self):
+		''' create order instance '''
+
 		user = self.request.user
 		item = self.order_items[0]
 		name = f'{item.item.name} 외 {len(self.order_items) - 1}건' \
@@ -28,3 +34,54 @@ class OrderInfo:
 		)
 
 		return instance
+
+
+class OrderItemHandler:
+	''' 주문상품을 handling하는 class '''
+
+	def __init__(self, order):
+		self.order = order
+		self.order_items = order.orderitem_order.all()
+
+	def create_order_items(self, order_items):
+		''' 주문상품을 bulk_create로 생성 '''
+
+		for order_item in order_items:
+			order_item.order = self.order
+			order_item.status = self.order.status
+
+		OrderItem.objects.bulk_create(order_items)
+
+	def set_paid_status(self):
+		''' 주문상품의 status를 order의 status(paid)로 변경 '''
+
+		if not self.order.status == 'exchange_return':
+			self.order_items.update(status=self.order.status)
+
+	def set_cancel_status(self):
+		''' order_item의 status를 주문취소로 변경 '''
+
+		self.order_items.update(status='cancelled')
+
+	def stock_minus_counter(self):
+		''' item 수량을 order_item 수만큼 차감 '''
+
+		if self.order.status == 'paid':
+			for order_item in self.order_items:
+				item = order_item.item
+				item.stock = int(item.stock) - int(order_item.quantity)
+
+				if item.stock == 0:
+					item.is_public = False
+				item.save()
+
+	def stock_plus_counter(self):
+		''' item 수량을 order_item 수만큼 가감 '''
+
+		for order_item in self.order_items:
+			item = order_item.item
+			item.stock = int(item.stock) + int(order_item.quantity)
+
+			if item.stock > 0:
+				item.is_public = True
+			item.save()
