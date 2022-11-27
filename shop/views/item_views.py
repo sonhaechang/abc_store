7ff987368fdf081b91f63fcc49408bd66592616c
shc5529 <shc5529@naver.com>
2022-11-28 01:43:24 +0900

    refactor: add type hints in shop views
    1. shop/views/item_views.py에 모든 function에 type hints 추가
    2. shop/views/review.views.py에 모든 function에 type hinys 추가

--- shop/views/item_views.py
+++ shop/views/item_views.py
@@ -1,3 +1,7 @@
+from typing import Any, Dict
+
+from django.db.models import Model, QuerySet
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views.generic import ListView
@@ -7,14 +11,17 @@ from core.mixins import PaginationsMixins
 from shop.models import Category, CategoryDetail, Item
 from shop.services import item_count
 
+
 # Create your views here.
 class CategoryListAllView(PaginationsMixins, ListView):
+	''' 카테고리별 모든 상세 카테고리의 상품 목록 '''
+
 	template_name = 'shop/container/category_list_all.html'
 
-	def get_object(self, **kwargs):
+	def get_object(self, **kwargs: Any) -> Model:
 		return Category.objects.get_or_none(slug=self.kwargs['category_slug'])
 
-	def get_categorys(self, **kwargs):
+	def get_categorys(self, **kwargs: Any) -> QuerySet[Any] | list[Any]:
 		category = self.get_object()
 
 		if category is not None:
@@ -23,11 +30,11 @@ class CategoryListAllView(PaginationsMixins, ListView):
 		else:
 			return list()
 
-	def get_queryset(self, **kwargs):
+	def get_queryset(self, **kwargs: Any) -> list[Model]:
 		return [item for category in self.get_categorys() for item in Item.objects.filter(
 			category=category).select_related('category').prefetch_related('itemimage_item')]
 		
-	def get_context_data(self, **kwargs):
+	def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
 		context = super().get_context_data(**kwargs)
 		context['category'] = self.get_object()
 		context['categorys'] = self.get_categorys()
@@ -37,19 +44,20 @@ class CategoryListAllView(PaginationsMixins, ListView):
 
 
 class CategoryListView(PaginationsMixins, ListView):
+	''' 카테고리에 상세 카데고리별 상품 목록 '''
+
 	model = CategoryDetail
 	template_name = 'shop/container/category_list.html'
 
-	def get_object(self, **kwargs):
+	def get_object(self, **kwargs: Any) -> Model:
 		return get_object_or_404(
-			self.model.objects.select_related('category'), 
-			slug=self.kwargs['sub_category_slug'])
+			self.model.objects.select_related('category'), slug=self.kwargs['sub_category_slug'])
 
-	def get_queryset(self, **kwargs):
+	def get_queryset(self, **kwargs: Any) -> list[Model]:
 		return [category for category in \
 			self.get_object().item_category.all().order_by('-id').prefetch_related('itemimage_item')]
 
-	def get_context_data(self, **kwargs):
+	def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
 		context = super().get_context_data(**kwargs)
 		category = self.get_object()
 		categorys = self.model.objects.prefetch_related(
@@ -62,8 +70,11 @@ class CategoryListView(PaginationsMixins, ListView):
 
 		return context
 
+def item_detail(request: HttpRequest, category_slug: str, 
+				sub_category_slug: str, item_slug: str) -> HttpResponse:
 
-def item_detail(request, category_slug, sub_category_slug, item_slug):
+	''' 상품 상세 '''			
+
 	item = get_object_or_404(
 		Item.objects.prefetch_related('review_item', 'itemimage_item'), slug=item_slug)
 
--- shop/views/review_views.py
+++ shop/views/review_views.py
@@ -1,15 +1,21 @@
+from typing import Any
+
+from django.db.models import Model, QuerySet
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse, HttpRequest 
 
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, DestroyModelMixin
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.response import Response
+from rest_framework.serializers import SerializerMetaclass
 
 from core.paginations import DefaultPagination
 
 from shop.models import Item, Review, ReviewImage
 from shop.serializers import ReviewSerializer
+
 
 
 class ReviewAPIView(ListModelMixin, CreateModelMixin, DestroyModelMixin, GenericAPIView):
@@ -21,30 +27,30 @@ class ReviewAPIView(ListModelMixin, CreateModelMixin, DestroyModelMixin, Generic
 	item_model = Item
 	review_model = Review
 
-	def get_object(self):
+	def get_object(self) -> Model:
 		''' 조건에 맞는 object을 반환 '''
 
 		return get_object_or_404(self.item_model, pk=self.kwargs['item_pk'])
 
-	def get_queryset(self):
+	def get_queryset(self) -> QuerySet[Any]:
 		''' 조건에 맞는 queryset을 반환 '''
 
 		return self.review_model.objects.filter(parent__isnull=True, item=self.get_object())
 		
-	def get_serializer_class(self):
+	def get_serializer_class(self) -> SerializerMetaclass:
 		''' serializer_class를 반환 '''
 
 		serializer = super().get_serializer_class()
 		serializer.user = self.request.user
 		return serializer
 
-	def get(self, request, *args, **kwargs):
+	def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> JsonResponse:
 		''' GET 요청을 처리하는 함수 '''
 
 		serializer = self.list(request, *args, **kwargs)
-		return  serializer
+		return serializer
 
-	def post(self, request, *args, **kwargs):
+	def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> JsonResponse:
 		''' POST 요청을 처리하는 함수 '''
 
 		data = request.data
@@ -67,7 +73,7 @@ class ReviewAPIView(ListModelMixin, CreateModelMixin, DestroyModelMixin, Generic
 		else:
 			print(serializer.errors)
 
-	def delete(self, request, *args, **kwargs):
+	def delete(self, request: HttpRequest, *args: Any, **kwargs: Any) -> JsonResponse:
 		''' DELETE 요청을 처리하는 함수 '''
 
 		review_pk = request.data.get('review_id')
