1d851c05d426ef50a0f22edf0ad71d9fd6c80f27
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2022-12-02 00:15:37 +0900

    feat: create cart models
    1. cart (장바구니 모델 생성), fields(필드정보): user(구매자), item(상품), quantity(수량), created_at(생성일), updated_at(수정일), deleted_at(삭제일)
    2. create cart models migrations files

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,45 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+
+from core.models import HistoryModel
+
+from shop.models import Item
+
 
 # Create your models here.
+class Cart(HistoryModel):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='%(class)s_user', 
+        on_delete=models.CASCADE,
+        verbose_name=_('구매자')
+    )
+
+    item = models.ForeignKey(
+        to='shop.Item', 
+        related_name='%(class)s_item', 
+        on_delete=models.CASCADE,
+        verbose_name=_('상품')
+    )
+
+    quantity = models.PositiveSmallIntegerField(
+        verbose_name=_('수량')
+    )
+
+    class Meta:
+        ordering = ['-id']
+        db_table = 'cart_tb'
+        verbose_name = _('장바구니')
+        verbose_name_plural = _('장바구니')
+        
+
+    def __str__(self):
+        return self.user.username
+
+    def total_amount(self):
+        if self.item.sale_amount:
+            return self.quantity * self.item.sale_amount
+        else:
+            return self.quantity * self.item.amount
