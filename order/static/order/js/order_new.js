67b836b3050e90cb1ade8375d9ab0b5dbffb3db9
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2022-12-10 17:33:29 +0900

    refector: order_new.js의 코드 function에서 class로 변경
    1. order_new.js의 코드 function에서 Order class로 변경
    2. Order에 select_items method 추가 (장바구니에서 checkbox 추가후 체크박스 선택시 총액 계산하는 기능)
    3. Order에 calculate_total_amount method 추가 (select_items method에서 계산된 총액 html에 표시하는 기능)
    4. Order에 constructor 추가 (this.cart_total_amount, this.items | select_items method에서 선택된 element와 계산된 총 금액 저장)
    5. Order에 order_new method 수정 (상품 목록 element 받아오는 parameter 제거하고 this.items 배열에 담긴 값으로 대체)
    6. Order class에 commaMixin 상속
    7. cart_list.html에 선택 상품 주문하기 버튼과, 상품별 각개 주문하기 버튼 제거 및 상품별 selectbox 추가 (선택된 상품으로만 주문 가능하게 수정)
    8. item_detail.html에 주문하기 버튼 삭제 장바구니 기능만 남기고 주문은 장바구니에서만 가능하게 수정

--- order/static/order/js/order_new.js
+++ order/static/order/js/order_new.js
@@ -1,23 +1,95 @@
-document.getElementById('order-new').addEventListener('click', e => {
-	const mapped = _.map(document.querySelectorAll('#item-list .item'), item => {
-		const item_id = item.getAttribute('data-item-id');
-		const quantity = 1;
-		return { item_id: item_id, quantity: quantity };
-	});
-
-	var filtered = _.filter(mapped, item => {
-		return item.quantity > 0;
-	});
-
-	if ( filtered.length > 0) {
-		const args = new URLSearchParams(_.object(_.map(filtered, _.values)));
-		const url = e.currentTarget.getAttribute('href') + '?' + args;
-		e.currentTarget.setAttribute('href', url);
-		return true;
-	}
-	else {
-		alert('상품 수량을 선택해주세요.');
+class Order {
+	constructor() {
+		this.cart_total_amount = 0;
+		this.items = new Array();
 	}
 
-	return false;
-});
+	mapped(ele) {
+		const mapped = _.map(ele, item => {
+			const item_id = item.getAttribute('data-item-id');
+			const quantity = 1;
+			return { item_id: item_id, quantity: quantity };
+		});
+
+		return mapped;
+	}
+
+ 	filtered(mapped) {
+		const filtered = _.filter(mapped, item => {
+			return item.quantity > 0;
+		});
+
+		return filtered;
+	}
+
+	check_quantity(e, filtered) {
+		if (filtered.length > 0) {
+			const args = new URLSearchParams(_.object(_.map(filtered, _.values)));
+			const url = e.currentTarget.getAttribute('href') + '?' + args;
+			e.currentTarget.setAttribute('href', url);
+			return true;
+		}
+		else {
+			alert('1개 이상의 상품을 선택해주세요.');
+		}
+	
+		return false;
+	}
+
+	calculate_total_amount() {
+		document.getElementById('cart-total').innerText = this.comma(this.cart_total_amount);
+	}
+
+	select_items(checkboxs) {
+		checkboxs.forEach(ele => {
+            ele.addEventListener('change', e => {
+				const item = e.target.parentNode.parentNode.parentNode;
+				const amount = parseInt(document.getElementById(
+					`total-amount-${e.target.getAttribute('data-id')}`).getAttribute('value'));
+
+				if (e.target.checked) {
+					this.cart_total_amount += amount;
+					this.calculate_total_amount();
+					this.items.push(item);
+				} else {
+					this.cart_total_amount -= amount;
+					this.calculate_total_amount();
+					this.items.splice(this.items.indexOf(item), 1);
+				}				
+            })
+        })
+		
+	}
+
+	order_new(e) {
+		const mapped = this.mapped(this.items);
+		const filtered = this.filtered(mapped);
+		return this.check_quantity(e, filtered);
+	}
+}
+
+Object.assign(Order.prototype, commaMixin);
+
+// document.getElementById('order-new').addEventListener('click', e => {
+// 	const mapped = _.map(document.querySelectorAll('#item-list .item'), item => {
+// 		const item_id = item.getAttribute('data-item-id');
+// 		const quantity = 1;
+// 		return { item_id: item_id, quantity: quantity };
+// 	});
+
+// 	var filtered = _.filter(mapped, item => {
+// 		return item.quantity > 0;
+// 	});
+
+// 	if ( filtered.length > 0) {
+// 		const args = new URLSearchParams(_.object(_.map(filtered, _.values)));
+// 		const url = e.currentTarget.getAttribute('href') + '?' + args;
+// 		e.currentTarget.setAttribute('href', url);
+// 		return true;
+// 	}
+// 	else {
+// 		alert('상품 수량을 선택해주세요.');
+// 	}
+
+// 	return false;
+// });
