d832d7d8af97d55a283f4edd030c5f2dee8dfe49
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2022-12-13 15:55:24 +0900

    refector: add order list link in navbar
    1. templates/components/navbar.html(메뉴바)에 주문 목록 링크 추가
    2. order/urls/urlspatterns에 path('list/', views.OrderListView.as_view(), name='order_new')에 name을 'order_list'로 수정

--- order/urls.py
+++ order/urls.py
@@ -3,7 +3,7 @@ from order import views
 
 app_name = 'order'
 urlpatterns = [
-	path('list/', views.OrderListView.as_view(), name='order_new'),
+	path('list/', views.OrderListView.as_view(), name='order_list'),
 	path('pay/', views.order_pay, name='order_pay'),
 	path('complete/<str:merchant_uid>/', views.order_complete, name='order_complete'),
 	path('detail/<str:merchant_uid>/', views.order_detail, name='order_detail'),
