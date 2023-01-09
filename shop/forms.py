78371823e2597978a4c63294201c87f931b4fabe
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-01-09 15:03:57 +0900

    feat: set option field initial data in AdminItemForm
    1. __init__ method를 overriding해서 생성된  ItemOption으로 dict type로 변환 및 json.dumps 후 options fields에 초기값 설정

--- shop/forms.py
+++ shop/forms.py
@@ -1,3 +1,5 @@
+import json
+
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
@@ -8,7 +10,7 @@ class AdminItemForm(forms.ModelForm):
     options = forms.CharField(
         # _(u'옵션 선택'),
         required=False,
-        widget=ItemOptionwidget
+        widget=ItemOptionwidget,
     )
 
     class Meta:
@@ -18,3 +20,18 @@ class AdminItemForm(forms.ModelForm):
             'supply_amount', 'amount', 'tax', 'sale_percent', 'sale_amount', 
             'stock', 'is_halal', 'is_public','best_item', 'options'
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        try:
+            item_dict = dict()
+            for item in self.instance.itemoption_item.all():
+                if item.name in item_dict:
+                    item_dict[item.name].append(item.value)
+                else:
+                    item_dict[item.name] = [item.value]
+
+            self.fields['options'].initial = json.dumps(item_dict)
+        except ValueError:
+            pass
