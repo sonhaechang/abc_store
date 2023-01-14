448fa40ec877f7aa10251ffae531668ee8ae6f83
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-01-14 13:16:47 +0900

    refector: modifiy options fields
    1. options fields 새로 생성하고 초기값 설정 부분 제거
    2. models에서 생성되어져 있는 options fields에 widgets 추가 (ItemOptionwidget)

--- shop/forms.py
+++ shop/forms.py
@@ -7,11 +7,6 @@ from shop.models import Item
 from shop.widgets import ItemOptionwidget
 
 class AdminItemForm(forms.ModelForm):
-    options = forms.CharField(
-        # _(u'옵션 선택'),
-        required=False,
-        widget=ItemOptionwidget,
-    )
 
     class Meta:
         models = Item
@@ -21,17 +16,4 @@ class AdminItemForm(forms.ModelForm):
             'stock', 'is_halal', 'is_public','best_item', 'options'
         ]
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        try:
-            item_dict = dict()
-            for item in self.instance.itemoption_item.all():
-                if item.name in item_dict:
-                    item_dict[item.name].append(item.value)
-                else:
-                    item_dict[item.name] = [item.value]
-
-            self.fields['options'].initial = json.dumps(item_dict)
-        except ValueError:
-            pass
+        widgets = {'options': ItemOptionwidget}
