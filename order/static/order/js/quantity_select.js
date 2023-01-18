096ab2ac4ba2dd10236086617c48b126a24c66ef
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-01-18 16:43:52 +0900

    refector: modify quantity_select.js in QuantitySelecter class
    1. totalAmountMixin 추가
    2. item_detail.html(상품 상세 페이지)에서 수량 버튼 클릭시 plus, minus_total_amount method로 총 (결제)금액 계산
    3. 일부 html의id, class, attr 이름 변경(html dom 수정에 맞춰서 변경)

--- order/static/order/js/quantity_select.js
+++ order/static/order/js/quantity_select.js
@@ -6,27 +6,38 @@ class QuantitySelecter {
     quantity_select(is_update=false) {
         this.factor_btn.forEach(ele => {
             ele.addEventListener('click', e => {
-                const id = e.currentTarget.getAttribute('data-id'),
-                    el = e.currentTarget.parentNode.children[1],
+                const id = e.currentTarget.getAttribute('data-id');
+                const cartId = e.currentTarget.getAttribute('data-cart-id');
+                const totalAmount = document.getElementById(`total-amount-${id}`) ? 
+                    document.getElementById(`total-amount-${id}`) : 
+                    document.getElementById(`total-amount-${cartId}`);
+
+                const el = e.currentTarget.parentNode.children[1],
                     factor = parseInt(e.currentTarget.getAttribute('data-factor')),
                     amount = e.currentTarget.getAttribute('value');
-                const totalAmount = document.getElementById(`total-amount-${id}`) ? 
-                    document.getElementById(`total-amount-${id}`) : document.getElementById(`total-amount`);
+                
                 let quantity = parseInt(el.innerHTML) + factor;
 
                 if (quantity < 1 ) { quantity = 1; }
 				el.innerHTML = quantity;
 
                 const total = amount * quantity;
+                
                 totalAmount.innerHTML = this.comma(total);
                 totalAmount.setAttribute('value', total);
 
                 if (is_update === true) {
-                    cart.update_quantity(id, factor);
+                    cart.update_quantity(e, factor);
+                } else {
+                    if (factor === 1) {
+                        this.plus_total_amount(amount);
+                    } else {
+                        this.minus_total_amount(amount);
+                    }
                 }
             })
         })
     }
 }
 
-Object.assign(QuantitySelecter.prototype, commaMixin);
+Object.assign(QuantitySelecter.prototype, commaMixin, totalAmountMixin);
