3cbcd5e52fcd31c660ae1d2d111a82f097ae344c
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-01-22 23:35:36 +0900

    refector: modify cart app services.py
    1. SessionCart 맨 아래로 이동 및 주석 처리
    2. CookieCart의 __iter__ 수정(cart에 저장된 상품을 item 정보에서 itemreal로 변경 및 itemreal에서 item정보 가져오기)
    3. CookieCart의 get_item_total 수정(item -> itemreal으로 변경 및 \
       amount를 item의 if문으로 sale_amount 유무로 amount 가져오던것을 item.get_amount()으로 변경)
    4. CookieCart의 add 수정(pk, quantiy 인자를 -> item_real_dict로 변경 및 cookie에 저장 방법 수정)
    5. cart_update_or_create 수정(item, cart_qs, qunatity 인자를 -> item_real_dict로 변경 및 db에 저장 방법 변경

--- cart/services.py
+++ cart/services.py
@@ -1,90 +1,17 @@
 import json
 from typing import Any, Union
 
-from django.conf import settings
-from django.db.models import F, Model, QuerySet
 from django.http import (
     HttpRequest, HttpResponse, HttpResponseRedirect,
     JsonResponse,
 )
+from django.shortcuts import get_object_or_404
 
 from cart.models import Cart
 
-from shop.models import Item
+from shop.models import Item, ItemReal
 
 RedirectOrResponse = Union[HttpResponseRedirect, HttpResponse, JsonResponse]
-
-
-class SessionCart(object):
-    ''' 세션 기반의 장바구니 클래스 (비로그인으로 장바구니 이용시 사용) '''
-
-    def __init__(self, request: HttpRequest) -> None:
-        ''' 초기화 '''
-
-        self.session = request.session
-
-        cart = self.session.get(settings.CART_ID)
-        self.cart = self.session[settings.CART_ID] = dict() if not cart else cart
-
-    def __len__(self) -> int:
-        ''' 세션 기반 장바구니에 저장 되어진 상품 전체 수량 반환 '''
-
-        return sum(item['quantity'] for item in self.cart.values())
-
-    def __iter__(self) -> dict[Any]:
-        ''' Yield 활용한 세션 기반 장바구니에 저장 되어진 상품들을 generator해서 반환 '''
-
-        for item in self.cart.values():
-            item['total_amount'] = item['item']['amount'] * item['quantity']
-            yield item
-
-    def save(self) -> None:
-        ''' 세션에 변동 사항(수정, 삭제)을 저장 '''
-
-        self.session[settings.CART_ID] = self.cart
-        self.session.modified = True
-
-    def add(self, item, quantity: int=1) -> None:
-        ''' 특정 상품을 세션 기반 장바구니에 저장 '''
-
-        item_id = str(item.id)
-
-        if item_id not in self.cart:
-            self.cart[item_id] = {'quantity': quantity} 
-            self.cart[item_id]['item'] = {
-                'pk': item.pk,
-                'name': item.name,
-                'amount': item.sale_amount if item.sale_amount else item.amount,
-                'get_first_image': item.itemimage_item.first(),
-                'is_public': item.is_public,
-            }
-
-        self.cart[item_id]['quantity'] += int(quantity)
-
-        if self.cart[item_id]['quantity'] <= 0:
-            self.cart[item_id]['quantity'] = 1
-
-        self.save()
-
-    def remove(self, item: Model) -> None:
-        ''' 특정 상품을 세션 기반 장바구니에서 삭제 '''
-
-        item_id = str(item.id)
-
-        if item_id in self.cart:
-            del(self.cart[item_id])
-            self.save()
-
-    def clear(self) -> None:
-        ''' 세션 기반 장바구니에 저장된 모든 상품 삭제 '''
-
-        self.session[settings.CART_ID] = dict()
-        self.session.modified = True
-
-    def get_item_total(self) -> int:
-        ''' 세션 기반 장바구니에 저장된 모든 상품의 총 금액 반환 '''
-
-        return sum(int(item['item']['amount']) * int(item['quantity']) for item in self.cart.values())
 
 
 class CookieCart(object):
@@ -104,8 +31,8 @@ class CookieCart(object):
         ''' Yield 활용한 쿠키 기반 장바구니에 저장 되어진 상품들을 generator해서 반환 '''
 
         for k, v in self.get_cookies().items():
-            item = Item.objects.get_or_none(pk=k)
-            amount = item.sale_amount if item.sale_amount else item.amount
+            item = ItemReal.objects.get_or_none(pk=k)
+            amount = item.get_amount()
 
             yield {
                 'pk': k,
@@ -114,8 +41,10 @@ class CookieCart(object):
                 'item': {
                     'pk': item.pk,
                     'name': item.name,
-                    'amount': amount,
-                    'get_first_image': item.itemimage_item.first(),
+                    'get_amount': amount,
+                    'extra_amount': item.extra_amount,
+                    'get_first_image': item.item.itemimage_item.first(),
+                    'get_item_name': item.get_item_name(),
                     'is_public': item.is_public,
                 }
             }
@@ -126,8 +55,8 @@ class CookieCart(object):
         item_amounts = list()
 
         for k, v in self.get_cookies().items():
-            item = Item.objects.get_or_none(pk=k)
-            amount = item.sale_amount if item.sale_amount else item.amount
+            item = ItemReal.objects.get_or_none(pk=k)
+            amount = item.get_amount()
             item_amounts.append(int(amount) * int(v))
 
         return sum(item_amounts)
@@ -153,15 +82,18 @@ class CookieCart(object):
 
         response.delete_cookie('cart_list')
 
-    def add(self, response: RedirectOrResponse, pk: str, quantity: str) -> None:
+    def add(self, response: RedirectOrResponse, item_real_dict: dict[Any]) -> None:
         ''' 특정 상품을 쿠키 장바구니에 저장 '''
 
         cart_list = self.get_cookies()
 
         if cart_list:
-            cart_list[pk] = int(cart_list[pk]) + int(quantity) if pk in cart_list else int(quantity)
+            for pk, quantity in item_real_dict.items():
+                quantity = int(cart_list[pk]) + int(quantity) if pk in cart_list else int(quantity)
+                cart_list[pk] = 1 if quantity <= 0 else quantity
         else:
-            cart_list = {pk: quantity}
+            for pk, quantity in item_real_dict.items():
+                cart_list[pk] = quantity
 
         self.set_cookie(response, cart_list)
 
@@ -175,16 +107,19 @@ class CookieCart(object):
 
         self.set_cookie(response, cart_list)
 
-def cart_update_or_create(request: HttpRequest, item: Model, cart_qs: QuerySet, quantity: str) -> None:
+def cart_update_or_create(request: HttpRequest, item_real_dict: dict[Any]) -> None:
     ''' 장바구니에 상품이 있으면 생성 없으면 추가 '''
 
-    if not cart_qs.exists():
-        Cart.objects.create(
-            user=request.user,
-            item=item,
-            quantity=int(quantity))
-    else:
-        for cart in cart_qs:
+    for pk, quantity in item_real_dict.items():
+        item = get_object_or_404(ItemReal, pk=pk)
+        cart = Cart.objects.get_or_none(user=request.user, item=item)
+        
+        if cart is None:
+            Cart.objects.create(
+                user=request.user,
+                item=item,
+                quantity=int(quantity))
+        else:
             quantity = cart.quantity + int(quantity)
             cart.quantity = 1 if int(quantity) < 1 else quantity
             cart.save()
@@ -198,3 +133,75 @@ def anonymous_user_cart_to_db_cart(request: HttpRequest, cart_list: dict[Any]) -
             quantity = int(cart_list[str(item.pk)])
 
             cart_update_or_create(request, item, cart_qs, quantity)
+
+
+# class SessionCart(object):
+#     ''' 세션 기반의 장바구니 클래스 (비로그인으로 장바구니 이용시 사용) '''
+
+#     def __init__(self, request: HttpRequest) -> None:
+#         ''' 초기화 '''
+
+#         self.session = request.session
+
+#         cart = self.session.get(settings.CART_ID)
+#         self.cart = self.session[settings.CART_ID] = dict() if not cart else cart
+
+#     def __len__(self) -> int:
+#         ''' 세션 기반 장바구니에 저장 되어진 상품 전체 수량 반환 '''
+
+#         return sum(item['quantity'] for item in self.cart.values())
+
+#     def __iter__(self) -> dict[Any]:
+#         ''' Yield 활용한 세션 기반 장바구니에 저장 되어진 상품들을 generator해서 반환 '''
+
+#         for item in self.cart.values():
+#             item['total_amount'] = item['item']['amount'] * item['quantity']
+#             yield item
+
+#     def save(self) -> None:
+#         ''' 세션에 변동 사항(수정, 삭제)을 저장 '''
+
+#         self.session[settings.CART_ID] = self.cart
+#         self.session.modified = True
+
+#     def add(self, item, quantity: int=1) -> None:
+#         ''' 특정 상품을 세션 기반 장바구니에 저장 '''
+
+#         item_id = str(item.id)
+
+#         if item_id not in self.cart:
+#             self.cart[item_id] = {'quantity': quantity} 
+#             self.cart[item_id]['item'] = {
+#                 'pk': item.pk,
+#                 'name': item.name,
+#                 'amount': item.sale_amount if item.sale_amount else item.amount,
+#                 'get_first_image': item.itemimage_item.first(),
+#                 'is_public': item.is_public,
+#             }
+
+#         self.cart[item_id]['quantity'] += int(quantity)
+
+#         if self.cart[item_id]['quantity'] <= 0:
+#             self.cart[item_id]['quantity'] = 1
+
+#         self.save()
+
+#     def remove(self, item: Model) -> None:
+#         ''' 특정 상품을 세션 기반 장바구니에서 삭제 '''
+
+#         item_id = str(item.id)
+
+#         if item_id in self.cart:
+#             del(self.cart[item_id])
+#             self.save()
+
+#     def clear(self) -> None:
+#         ''' 세션 기반 장바구니에 저장된 모든 상품 삭제 '''
+
+#         self.session[settings.CART_ID] = dict()
+#         self.session.modified = True
+
+#     def get_item_total(self) -> int:
+#         ''' 세션 기반 장바구니에 저장된 모든 상품의 총 금액 반환 '''
+
+#         return sum(int(item['item']['amount']) * int(item['quantity']) for item in self.cart.values())
